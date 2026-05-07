432aa5a338f227debd0686c943c3fae15187180a
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-05-07 15:03:08 +0000

    Mais alterações

--- Projetos JavaScript/conversoes.js
+++ Projetos JavaScript/conversoes.js
@@ -16,3 +16,8 @@ console.log(numero + numeroString)
 //Calculadora inteligente
 
 const preco = "100.50";
+const desconto = "10";
+
+console.log(preco - desconto);
+console.log(preco * 2);
+console.log(preco / 2);
