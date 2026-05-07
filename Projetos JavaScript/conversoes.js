57248acf6e58197f99741c37c52dd42ca512dcc1
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-05-07 11:51:00 -0300

    mais organização

---
+++ Projetos JavaScript/conversoes.js
@@ -0,0 +1,18 @@
+//tipo de dados
+//booleanos
+
+//conversão implicita
+
+const numero = 456;
+const numeroString = "(456)"
+
+// Number()
+// String()
+
+console.log(numero + numeroString)
+
+//conversão explícita
+
+//Calculadora inteligente
+
+const preco = "100.50";
--- Projetos JavaScript/ternario.js
+++ Projetos JavaScript/ternario.js
@@ -1,2 +1,9 @@
 const idadeMinima = 18;
 const idadeCliente = 16;
+
+
+if (idadeCliente >= idadeMinima){
+    console.log("cerveja")
+} else {
+    console.log("suco")
+}
