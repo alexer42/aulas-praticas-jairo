1ed7e51c152fca108176904de54cab7758a14597
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-05-14 14:35:40 +0000

    .

---
+++ Projetos JavaScript/concatenacao.js
@@ -0,0 +1,9 @@
+const nome = "Alexandre";
+const dataNascimento = 2010;
+const dataAtual = 2026;
+const idade = 16;
+const cidadeNascimento = "Roncador";
+
+const apresentacao = 'Meu nome é {nome}'
+
+console.log(apresentacao);
