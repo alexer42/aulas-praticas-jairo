480b9b0c908b1a09fd2f188e8d98e8195c5d18b1
alexer42 <alexandre.dziubate@escola.pr.gov.br>
2026-07-31 15:07:34 +0000

    .

---
+++ callback.js
@@ -0,0 +1,21 @@
+const nomes = ["Evaldo", "Mari", "Camis"]
+
+
+/*
+EXEMPLOS
+
+nomes.forEach( function(nome){
+    console.log(nome)
+});
+
+ nomes.forEach((nome) => {
+    console.log(nome)
+}); 
+
+*/
+
+function imprimeNome(nome){
+    console.log(nome)
+};
+
+nomes.forEach(imprimeNome);
